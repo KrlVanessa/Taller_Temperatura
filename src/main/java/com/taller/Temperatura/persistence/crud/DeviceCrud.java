e73f7d678ee150dcbed8ef8ef4d47f0a1ec17adc
KrlVanessa <124644878+KrlVanessa@users.noreply.github.com>
2024-08-31 01:19:46 -0500

    se creo de la TABLA device: entidad, crud y repositorio.

---
+++ src/main/java/com/taller/Temperatura/persistence/crud/DeviceCrud.java
@@ -0,0 +1,19 @@
+package com.taller.Temperatura.persistence.crud;
+
+import com.taller.Temperatura.persistence.entity.Device;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DeviceCrud extends CrudRepository<Device, Long> {
+
+    // Método para bscar dispositivos por nombre
+    Optional<Device> findByName(String name);
+
+    // Método para bscar dispositivos por ubicación
+    List<Device> findByLocation(String location);
+
+    // Método adicional bscar eliminar dispositivos por nombre
+    void deleteByName(String name);
+}
---
+++ src/main/java/com/taller/Temperatura/persistence/repository/DeviceRepository.java
@@ -0,0 +1,31 @@
+package com.taller.Temperatura.persistence.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.taller.Temperatura.persistence.crud.DeviceCrud;
+import com.taller.Temperatura.persistence.entity.Device;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class DeviceRepository {
+    private final DeviceCrud deviceCrud;
+
+    @Autowired
+    public DeviceRepository(DeviceCrud deviceCrud) {
+        this.deviceCrud = deviceCrud;
+    }
+
+    public Optional<Device> searchByName(String name) {
+        return deviceCrud.findByName(name);
+    }
+
+    public List<Device> searchByLocation(String location) {
+        return deviceCrud.findByLocation(location);
+    }
+
+    public void deleteByName(String name) {
+        deviceCrud.findByName(name).ifPresent(deviceCrud::delete);
+    }
+}
